d4cf98b82e895fcd5a1de58a37f866bc298643ce
rzanabusamra22 <rzanabusamra@gmail.com>
2020-12-29 21:13:05 +0200

    Styling

--- frontend/src/components/aboutSection/index.js
+++ frontend/src/components/aboutSection/index.js
@@ -6,8 +6,18 @@ const aboutSection = () => {
     return (
         <>
             <main class='l-main' id='about'>
-                 <section class='about' >
-                         About
+                 <section class='about section' >
+                         <span class='section-subtitle'>My intro</span>
+                         <h2 class="section-title">About Me</h2>
+                         <div class="about__container bd-grid">
+                             <div class="about__data">
+                                 <p class="about__description">
+                                 The most effective people are those who understand their strengths. 
+                                 Here are my top 5: Futuristic – Empathy – Discipline – Significance – Responsibility.
+                                 (As a result of-The Clifton Strengths Assessment-Gallup)
+                                 </p>
+                             </div>
+                         </div>
                  </section>
             </main>
         </>
