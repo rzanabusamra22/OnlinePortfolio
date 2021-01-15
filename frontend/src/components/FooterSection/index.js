d8a8af2bd87d8890d165d027c2746d097fc7f852
rzanabusamra22 <rzanabusamra@gmail.com>
2021-01-15 20:10:08 +0200

    Adding Data

--- frontend/src/components/FooterSection/index.js
+++ frontend/src/components/FooterSection/index.js
@@ -6,15 +6,14 @@ const footerSection = () => {
         <>
             <footer class="footer section">
                 <div class="footer__container bd-grid">
-                    <h1 class="footer__title">Razan Abusamra</h1>
-                    <p class="footer__description">You Can Find Me On</p>
+                 <h1 class="footer__title">Razan Abusamra</h1>
+                 <p class="footer__description">You Can Find Me On</p>
                     <div class="footer__social">
-                    <a href="https://www.facebook.com/razank.abusamra/" class="footer__link"><span class="iconify" data-icon="bx:bxl-facebook-circle" data-inline="false"></span></a>
-                    <a href="https://www.instagram.com/rzan.abusamra/" class="footer__link"><span class="iconify" data-icon="entypo-social:instagram-with-circle" data-inline="false"></span></a>
-                    <a href="https://twitter.com/RazankAbuSamra" class="footer__link"><span class="iconify" data-icon="entypo-social:twitter-with-circle" data-inline="false"></span></a>
-                    <a href="https://api.whatsapp.com/send/?phone=972597476792&text&app_absent=0" class="footer__link"><span class="iconify" data-icon="dashicons:whatsapp" data-inline="false"></span></a>
-                </div>
-                <p class="footer__copy">All rights reserved 2021</p>
+                      <a href="https://www.facebook.com/razank.abusamra/" class="footer__link"><span class="iconify" data-icon="bx:bxl-facebook-circle" data-inline="false"></span></a>
+                      <a href="https://www.instagram.com/rzan.abusamra/" class="footer__link"><span class="iconify" data-icon="entypo-social:instagram-with-circle" data-inline="false"></span></a>
+                      <a href="https://twitter.com/RazankAbuSamra" class="footer__link"><span class="iconify" data-icon="entypo-social:twitter-with-circle" data-inline="false"></span></a>
+                      <a href="https://api.whatsapp.com/send/?phone=972597476792&text&app_absent=0" class="footer__link"><span class="iconify" data-icon="dashicons:whatsapp" data-inline="false"></span></a>
+                    </div>
                 </div>
                 
             </footer>
