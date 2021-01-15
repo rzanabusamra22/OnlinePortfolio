ecf0a8b47f695e4c35d20e322788db0244cac030
rzanabusamra22 <rzanabusamra@gmail.com>
2021-01-15 19:07:58 +0200

    Styling

--- frontend/src/components/HeroSection/index.js
+++ frontend/src/components/HeroSection/index.js
@@ -9,16 +9,16 @@ const HeroSection = () => {
                           <div class='home__data'>
                               <div class='home__img'>
                                   {/* picture */}
-                                  <img src='' alt=''>
+                                  <img class='home__img'src='https://i.imgur.com/7fdzTPp.jpg' alt=''>
                                   </img>
                               </div>
                               <h1 class='home__title'>Razan Abusamra</h1>
-                              <span class='home__profession'>Fulltime Dreamer</span>
+                              <span class='home__profession'>Fulltime Dreamer & Achiever</span>
                               <div class='home__social'>
                                   <a href='https://www.linkedin.com/in/razan-abu-samra/' class='home__social-link'><span class="iconify" data-icon="bx-bxl-linkedin" data-inline="false"></span></a>
                                   <a href='https://github.com/rzanabusamra22' class='home__social-link'><span class="iconify" data-icon="teenyicons:github-outline" data-inline="false"></span></a>
                               </div>
-                               <a href='#' download='' class='button'>Download Resume</a>
+                               <a href='https://docs.google.com/document/d/1rPufxWROWqbSfvy6m9dZCLc_iZuWTDxxzHrM7N46cpo/edit?usp=sharing' download='' class='button'>Resume</a>
                           </div>
                       </div>
                  </section>
