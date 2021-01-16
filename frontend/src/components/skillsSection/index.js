1f99638b175163218ab5a3157e6eb591451f2c1b
rzanabusamra22 <rzanabusamra@gmail.com>
2021-01-16 09:58:17 +0200

    Adding Data

--- frontend/src/components/skillsSection/index.js
+++ frontend/src/components/skillsSection/index.js
@@ -10,34 +10,31 @@ const skillsSection = ({id}) => {
                      <h2 class="section-title">My Expertise Area</h2>
                      <div class="skills__container bd-grid">
                          <div class="skills__content">
-                             <h3 class="skills__subtitle">Skill 1</h3>
+                             <h3 class="skills__subtitle">Frontend</h3>
                              <div class="skills__data">
-                                 <span class="skills_name">type</span>
-                                 <span class="skills_number">xx%</span>
+                                 <span class="skills_name">HTML/CSS</span>
+                                 <span class="skills_number">65%</span>
                                  <span class="skills_bar skills__html"></span>
                              </div>
                          </div>
                          <div class="skills__content">
-                             <h3 class="skills__subtitle">Skill 2</h3>
                              <div class="skills__data">
-                                 <span class="skills_name">type</span>
-                                 <span class="skills_number">xx%</span>
+                                 <span class="skills_name">jAVASCRIPT</span>
+                                 <span class="skills_number">75%</span>
                                  <span class="skills_bar skills__js"></span>
                              </div>
                          </div>
                          <div class="skills__content">
-                             <h3 class="skills__subtitle">Skill 3</h3>
                              <div class="skills__data">
-                                 <span class="skills_name">type</span>
-                                 <span class="skills_number">xx%</span>
+                                 <span class="skills_name">REACT</span>
+                                 <span class="skills_number">70%</span>
                                  <span class="skills_bar skills__react"></span>
                              </div>
                          </div>
                          <div class="skills__content">
-                             <h3 class="skills__subtitle">Skill 4</h3>
                              <div class="skills__data">
-                                 <span class="skills_name">type</span>
-                                 <span class="skills_number">xx%</span>
+                                 <span class="skills_name">RESPONSIVE DESIGN</span>
+                                 <span class="skills_number">60%</span>
                                  <span class="skills_bar skills__angular"></span>
                              </div>
                          </div>
